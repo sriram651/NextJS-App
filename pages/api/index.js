78c431e4b25a69c9c6d344133369bb3e9ce74352
--replace-all <--replace-all>
2023-05-14 17:01:23 +0530

    Pagination-Delete Icon-Company filter-perpagecount

--- pages/api/index.js
+++ pages/api/index.js
@@ -1,5 +1,5 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-import {json} from "/data/persondata.json";
+import {json} from "data/persondata.json";
 
 export default function handler(req, res) {
   res.status(200).json(json)
